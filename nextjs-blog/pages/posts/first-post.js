44303ee9e5222b44da7f4abfeb3ab6fb0075247d
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-10-09 18:42:35 +0100

    following the next.js starter docs

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,10 +1,17 @@
-import Link from "next/link";
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
 
-export default function FirstPost(){
+export default function FirstPost() {
     return (
-    <>
-    <h1>First Post</h1>
-        <Link href='/'>Link back home</Link>
-    </>
-    )
+      <>
+        <Head>
+          <title>FirstPost</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">← Back to home</Link>
+        </h2>
+      </>
+    );
 }
