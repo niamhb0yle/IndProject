d57d06c2b72ad62faeeeff23f61a0fc9125c35ee
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-10-09 18:18:38 +0100

    created new nextjs project, now has correct routing

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FirstPost(){
+    return (
+    <>
+    <h1>First Post</h1>
+        <Link href='/'>Link back home</Link>
+    </>
+    )
+}
