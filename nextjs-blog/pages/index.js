1347fd1a46f46c97068dab95181a11a27f883eb2
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-10-10 15:41:00 +0100

    homepage looks decent

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -16,12 +16,12 @@ export default function Home() {
           Curo.
         </h1>
 
-        <ul className={styles.startScreenList}>
-          <li>
-            <Link href='/login'>Log in</Link>
+        <ul className={styles.navbar}>
+          <li id={styles.navbarelements}>
+            <Link href='/login' id={styles.navbarelements}>Log in</Link>
           </li>
-          <li>
-            <Link href='/signup'>Sign up</Link>
+          <li id={styles.navbarelements}>
+            <Link href='/signup' id={styles.navbarelements}>Sign up</Link>
           </li>
         </ul>
 
@@ -30,42 +30,8 @@ export default function Home() {
 
       <style jsx>{`
         main {
-          padding: 5rem 0;
-          flex: 1;
+          padding: 2rem 0;
           display: block;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
         }
       `}</style>
 
