9029ad592dc58431b5069dbfa939199addb7d7ed
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-10-12 12:59:49 +0100

    adding basic input boxes

--- nextjs-blog/pages/login.js
+++ nextjs-blog/pages/login.js
@@ -1,17 +1,77 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import "@fontsource/montserrat"; 
 
-export default function SignUp() {
-    return (
-      <>
-        <Head>
-          <title>Log in</title>
-        </Head>
-        <h1>Log in</h1>
-        <h2>
-          <Link href="/">← Back to home</Link>
-        </h2>
-      </>
-    );
+export default function LogIn() {
+
+  function handleSubmit(e){
+    e.preventDefaullt()
+   }
+
+  return (
+    <div>
+      <Head>
+        <title>Curo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.container}>
+
+        <label htmlFor="#name">Email address</label>
+            <input className={`${styles.input}`}
+              placeholder="Email address"
+              type={"text"}
+              id="email"
+              name="email"
+            />
+
+
+        <label htmlFor="#address">Password</label>
+        <input className={`${styles.input}`}
+          placeholder="Password"
+          type={"text"}
+          id="password"
+          name="password"
+        />
+
+        <p>Don't have an account?</p>
+        <Link href='/signup' id={styles.navbarelements}>Sign up</Link>
+
+        
+      </main>
+
+      <style jsx>{`
+        main {
+          padding: 2rem 0;
+          display: block;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
 }
+
