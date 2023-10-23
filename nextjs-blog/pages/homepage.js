68a33114e687da20bf9d5dbe97968f3949589801
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-10-23 14:49:43 +0100

    large changes to UI, added skeleton for a few more pages

---
+++ nextjs-blog/pages/homepage.js
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import "@fontsource/montserrat"; 
+
+
+export default function Homepage() {
+  return (
+    <div>
+      <Head>
+        <title>Curo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.container}>
+        <h1>homepage</h1>        
+      </main>
+
+      <style jsx>{`
+        main {
+          padding: 2rem 0;
+          display: block;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
+
--- nextjs-blog/pages/login.js
+++ nextjs-blog/pages/login.js
@@ -3,9 +3,9 @@ import Image from 'next/image';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import "@fontsource/montserrat"; 
+import '@fontsource-variable/karla';
 
 export default function LogIn() {
-
   function handleSubmit(e){
     e.preventDefaullt()
    }
@@ -19,26 +19,19 @@ export default function LogIn() {
 
       <main className={styles.container}>
 
-        <label htmlFor="#name">Email address</label>
-            <input className={`${styles.input}`}
-              placeholder="Email address"
-              type={"text"}
-              id="email"
-              name="email"
-            />
-
-
-        <label htmlFor="#address">Password</label>
-        <input className={`${styles.input}`}
-          placeholder="Password"
-          type={"text"}
-          id="password"
-          name="password"
-        />
-
-        <p>Don't have an account?</p>
-        <Link href='/signup' id={styles.navbarelements}>Sign up</Link>
-
+        <div className="LogIn" style={{width: 1280, height: 832, position: 'relative'}}>
+          <div className="LogIn" style={{left: 199, top: 208, position: 'absolute', color: '#0051CB', fontSize: 68, fontFamily: 'Montserrat', fontWeight: '600', letterSpacing: 2.04, wordWrap: 'break-word'}}>Log in</div>
+          <div className="Group1" style={{width: 263, height: 61, left: 188, top: 538, position: 'absolute'}}>
+          <div className="Rectangle2" style={{width: 263, height: 61, left: 0, top: 0, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 30}} />
+          <div className="ContinueWithGoogle" style={{left: 57, top: 21, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Montserrat', fontWeight: '600', letterSpacing: 0.48, wordWrap: 'break-word'}}>Continue with google</div>
+        </div>
+        <div className="EmailAddress" style={{width: 106, height: 21, left: 154, top: 327, position: 'absolute', color: 'rgba(0, 0, 0, 0.40)', fontSize: 16, fontFamily: 'Karla Variable', fontWeight: '400', wordWrap: 'break-word'}}>Email Address</div>
+        <div className="Password" style={{width: 76, height: 18, left: 155, top: 420, position: 'absolute', color: 'rgba(0, 0, 0, 0.40)', fontSize: 16, fontFamily: 'Karla Variable', fontWeight: '400', wordWrap: 'break-word'}}>Password</div>
+        <div className="Rectangle10" style={{width: 319, height: 48, left: 153, top: 354, position: 'absolute', borderRadius: 8, border: '1px #CDCDCD solid'}} />
+        <div className="Rectangle11" style={{width: 317, height: 48, left: 155, top: 444, position: 'absolute', borderRadius: 8, border: '1px #CDCDCD solid'}} />
+        <div className="PhEyeSlashDuotone" style={{width: 21, height: 21, left: 453, top: 457, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: '0 0'}}>
+          </div>
+      </div>
         
       </main>
 
