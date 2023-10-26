b9d0292f73678d2b006639f28633bdea5599c4db
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-10-26 15:00:21 +0100

    finished sidebar with dummy links

--- curo/pages/homepage.js
+++ curo/pages/homepage.js
@@ -2,7 +2,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import "@fontsource/montserrat"; 
+import "@fontsource/montserrat";
+import '@fontsource-variable/karla';
+import "@fontsource/manrope";
 
 
 export default function Homepage() {
@@ -13,9 +15,18 @@ export default function Homepage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <body style={{background:'#DBE9FF', backgroundSize: 'cover', marginTop:0}}>
-        <div className={styles.sidebar}></div>    
-      </body>
+      <main style={{background:'#DBE9FF', backgroundSize: 'cover', marginTop:0, padding:0}}>
+        <div className={styles.sidebarContainer}>
+          <div id={styles.sidebarLogo}>Curo</div>
+          <div className={styles.sidebarNav}>
+            <Link id={styles.sidebarNavElements} href='/homepage'>Dashboard</Link>
+            <Link id={styles.sidebarNavElements} href='/homepage'>Teams</Link>
+            <Link id={styles.sidebarNavElements} href='/homepage'>Squad Members</Link>
+            <Link id={styles.sidebarNavElements} href='/homepage'>Goals</Link>
+            <Link id={styles.sidebarNavElements} href='/homepage'>Settings</Link>
+          </div>
+        </div>
+      </main>
 
       <style jsx>{`
         main {
