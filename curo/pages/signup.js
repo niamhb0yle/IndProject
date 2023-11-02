bdadf08237c3cc00d23b96151c4cc3e840500f4f
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-11-02 17:50:39 +0000

    firebase auth integrated (very basic)

--- curo/pages/signup.js
+++ curo/pages/signup.js
@@ -30,12 +30,15 @@ export default function SignUp() {
       try {
         if (role === 'dev'){
           // firebase auth
-          createUserWithEmailAndPassword(auth, userProfile.email, userProfile.password).then((userCredential) => {
-            const user = userCredential.user;
-          }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-          });
+          createUserWithEmailAndPassword(auth, userProfile.email, userProfile.password);
+            /*
+            .then((userCredential) => {
+              const user = userCredential.user;
+            }).catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+            });
+            */
 
           // firestore
           const docRef = await addDoc(collection(db, "developers"), {
@@ -45,12 +48,29 @@ export default function SignUp() {
           
           // console output
           console.log("Document written with ID: ", docRef.id);
+
         } else if (role === 'sm') {
-          const docRef = await addDoc(collection(db, "coaches"), {
-            email: userProfile.email,
-            username: userProfile.username,
-          });
-          console.log("Document written with ID: ", docRef.id);
+          if (role === 'dev'){
+            // firebase auth
+            createUserWithEmailAndPassword(auth, userProfile.email, userProfile.password);
+            /*
+            .then((userCredential) => {
+              const user = userCredential.user;
+            }).catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+            });
+            */
+  
+            // firestore
+            const docRef = await addDoc(collection(db, "developers"), {
+              email: userProfile.email,
+              username: userProfile.username,
+            });
+            
+            // console output
+            console.log("Document written with ID: ", docRef.id);  
+          }
         }
       } catch (e) {
         console.error("Error adding document: ", e);
