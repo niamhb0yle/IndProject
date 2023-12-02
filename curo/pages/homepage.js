c4aac0a6fe9d501225ae863e0de8ffa615ee01c3
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-12-02 19:20:07 +0000

    some of the dimension teasers spaced out and added to code

--- curo/pages/homepage.js
+++ curo/pages/homepage.js
@@ -37,6 +37,16 @@ export default function Homepage() {
 
             <div className={styles.dashboardContent}>
               <p>Here is a description of the team, with an introduction to their work. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              
+              <div className={styles.dimensionParentFlex}>
+                <div className={styles.dimensionTeaser}></div>
+                <div className={styles.dimensionTeaser}></div>
+                <div className={styles.dimensionTeaser}></div>
+                <div className={styles.dimensionTeaser}></div>
+                <div className={styles.dimensionTeaser}></div>
+                <div className={styles.dimensionTeaser}></div>
+              </div>
+
             </div>
 
           </div>
