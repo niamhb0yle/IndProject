f5294fee6debda27ff1fd116da320fbe7bac466a
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-12-10 01:55:29 +0000

    added more styling to dashboard component

--- curo/components/dashboard.js
+++ curo/components/dashboard.js
@@ -12,13 +12,24 @@ export default function Dashboard() {
         <p>Here is a description of the team, with an introduction to their work. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         
         <div className={styles.dimensionParentFlex}>
-            <Link href='/dimension-pages/technical' className={styles.dimensionTeaser} style={{background:'#F6866A'}}>Technical</Link>
-            <Link href='/dimension-pages/environmental' className={styles.dimensionTeaser} style={{background:'#7661E2'}}></Link>
-            <Link href='/dimension-pages/technical' className={styles.dimensionTeaser} style={{background:'#FABE7A'}}></Link>
-            <Link href='/dimension-pages/technical' className={styles.dimensionTeaser} style={{background:'#D8F4B4'}}></Link>
-            <Link href='/dimension-pages/technical' className={styles.dimensionTeaser} style={{background:'#9DC1F8'}}></Link>
-            <Link href='/dimension-pages/technical' className={styles.dimensionTeaser} style={{background:'#F6866A'}}></Link>
-            <Link href='/dimension-pages/technical' className={styles.dimensionTeaser} style={{background:'#F6866A'}}></Link>
+            <Link href='/dimension-pages/technical' className={styles.dimensionTeaser} style={{background:'#F6866A'}}>
+                <div className={styles.teaserText}>Technical</div>
+            </Link>
+            <Link href='/dimension-pages/environmental' className={styles.dimensionTeaser} style={{background:'#7661E2'}}>
+                <div className={styles.teaserText}>Environmental</div>
+            </Link>
+            <Link href='/dimension-pages/social' className={styles.dimensionTeaser} style={{background:'#FABE7A'}}>
+                <div className={styles.teaserText}>Social</div>
+            </Link>
+            <Link href='/dimension-pages/economic' className={styles.dimensionTeaser} style={{background:'darkgreen'}}>
+                <div className={styles.teaserText}>Economic</div>
+            </Link>
+            <Link href='/dimension-pages/individual' className={styles.dimensionTeaser} style={{background:'#9DC1F8'}}>
+                <div className={styles.teaserText}>Individual</div>
+            </Link>
+            <Link href='/dimension-pages/technical' className={styles.dimensionTeaser} style={{background:'#F6866A'}}>
+                <div className={styles.teaserText}>Placeholder</div>
+            </Link>
         </div>
 
     </div>
---
+++ curo/pages/dimension-pages/individual.js
@@ -0,0 +1,72 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
+import "@fontsource/montserrat";
+import '@fontsource-variable/karla';
+import "@fontsource/manrope";
+import SideBar from '../../components/sidebar';
+
+export default function Homepage() {
+
+  return (
+    <div>
+      <Head>
+        <title>Curo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main style={{background:'#DBE9FF', backgroundSize: 'cover', marginTop:0, padding:0}}>
+        <div className={styles.mainContainer}>
+          <SideBar/>
+
+          <div className={styles.dashboard}>
+            <div className={styles.dashboardHeader}>
+              <div id={styles.dashboardTitle}>CDAP Squad</div>
+              <Link id={styles.teamNavElements} href='/homepage'>Dashboard</Link>
+              <Link id={styles.teamNavElements} href='/issueBoard'>Sustainable Goals</Link>
+              <Link id={styles.teamNavElements} href='/reports'>Reports</Link>
+              <Link id={styles.teamNavElements} href='/teamSettings'>Team Settings</Link>
+            </div>
+
+            <div className={styles.dashboardContent}>
+              <p>Individual page.</p>
+              
+
+            </div>
+
+          </div>
+        </div>
+      </main>
+
+      <style jsx>{`
+        main {
+          padding: 2rem 0;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
+
