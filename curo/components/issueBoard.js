f474c0b2c9647a9ac5903c40f8d21bb081b4cba1
Niamh Boyle <2566489b@student.gla.ac.uk>
2023-12-27 02:46:35 +0000

    basic kanban board styling done, need to make into components

--- curo/components/issueBoard.js
+++ curo/components/issueBoard.js
@@ -22,16 +22,36 @@ export default function IssueBoard() {
     <div style={{display:'flex', flex: 1, flexDirection: 'row', flexWrap:'wrap', alignContent:'flex-start'}}>
         <div style={{width: 400, minHeight:600, background:'#d2cccc', marginRight:30, borderRadius:20, display:'flex', flex:1, alignItems:'center', flexDirection:'column', justifyContent:'flex-start', borderLeft:10, borderTop:10}}>
             <p style={{fontSize:25, fontFamily:'Manrope', fontWeight:1000, color: 'black'}}>To do:</p>  
-            <div style={{width:350, height:100, background:'linear-gradient(180deg, white 0%, white 60%, #8ae5c9 60%, #8ae5c9 100%)', borderRadius:20, marginBottom: 8, border:10}}><p>issue 1</p></div>
-            <div style={{width:350, height:100, background:'#8ae5c9', borderRadius:20, marginBottom: 8}}>issue 2</div>
-            <div style={{width:350, height:100, background:'#8ae5c9', borderRadius:20, marginBottom: 8}}>issue 3</div>
-            <div style={{width:350, height:100, background:'#8ae5c9', borderRadius:20, marginBottom: 8}}>issue 4</div>
+            <div style={{width:350, height:'fit-content', background:'linear-gradient(180deg, white 0%, white 60%, #6AB4B9 60%, #6AB4B9 100%)', borderRadius:20, marginBottom: 18}}>
+              <p style={{marginLeft:7, fontWeight:1000, fontSize:22}}>Issue 1 Title</p>
+              <p style={{marginLeft:7, fontSize:15}}>Assignees: Person 1, Person 2</p>
+            </div>
+            <div style={{width:350, height:'fit-content', background:'linear-gradient(180deg, white 0%, white 60%, #1e868d 60%, #1e868d 100%)', borderRadius:20, marginBottom: 18}}>
+              <p style={{marginLeft:7, fontWeight:1000, fontSize:22}}>Issue 2 Title</p>
+              <p style={{marginLeft:7, fontSize:15}}>Assignees: Person 1, Person 2</p>
+            </div>
+            <div style={{width:350, height:'fit-content', background:'linear-gradient(180deg, white 0%, white 60%, #155e63 60%, #155e63 100%)', borderRadius:20, marginBottom: 18}}>
+              <p style={{marginLeft:7, fontWeight:1000, fontSize:22}}>Issue 3 Title</p>
+              <p style={{marginLeft:7, fontSize:15}}>Assignees: Person 1, Person 2</p>
+            </div>
         </div>
-        <div style={{width: 400, minHeight:600, background:'#d2cccc', marginRight:30, borderRadius:20, display:'flex', flex:1, justifyContent:'center', border:5}}>
+        <div style={{width: 400, minHeight:600, background:'#d2cccc', marginRight:30, borderRadius:20, display:'flex', flex:1, alignItems:'center', flexDirection:'column', justifyContent:'flex-start', borderLeft:10, borderTop:10}}>
             <p style={{fontSize:25, fontFamily:'Manrope', fontWeight:1000, color: 'black'}}>In progress:</p>
+            <div style={{width:350, height:'fit-content', background:'linear-gradient(180deg, white 0%, white 60%, #6AB4B9 60%, #6AB4B9 100%)', borderRadius:20, marginBottom: 18}}>
+              <p style={{marginLeft:7, fontWeight:1000, fontSize:22}}>Issue 1 Title</p>
+              <p style={{marginLeft:7, fontSize:15}}>Assignees: Person 1, Person 2</p>
+            </div>
         </div>
-        <div style={{width: 400, minHeight:600, background:'#d2cccc', marginRight:30, borderRadius:20, display:'flex', flex:1, justifyContent:'center', border:5}}>
+        <div style={{width: 400, minHeight:600, background:'#d2cccc', marginRight:30, borderRadius:20, display:'flex', flex:1, alignItems:'center', flexDirection:'column', justifyContent:'flex-start', borderLeft:10, borderTop:10}}>
             <p style={{fontSize:25, fontFamily:'Manrope', fontWeight:1000, color: 'black'}}>Complete:</p>
+            <div style={{width:350, height:'fit-content', background:'linear-gradient(180deg, white 0%, white 60%, #6AB4B9 60%, #6AB4B9 100%)', borderRadius:20, marginBottom: 18}}>
+              <p style={{marginLeft:7, fontWeight:1000, fontSize:22}}>Issue 1 Title</p>
+              <p style={{marginLeft:7, fontSize:15}}>Assignees: Person 1, Person 2</p>
+            </div>
+            <div style={{width:350, height:'fit-content', background:'linear-gradient(180deg, white 0%, white 60%, #1e868d 60%, #1e868d 100%)', borderRadius:20, marginBottom: 18}}>
+              <p style={{marginLeft:7, fontWeight:1000, fontSize:22}}>Issue 2 Title</p>
+              <p style={{marginLeft:7, fontSize:15}}>Assignees: Person 1, Person 2</p>
+            </div>
         </div>
 
     </div>
