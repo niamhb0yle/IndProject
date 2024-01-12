a95de01325d5dee294b9d4edb4f2f8525dd7f1b4
Niamh Boyle <2566489b@student.gla.ac.uk>
2024-01-12 17:43:55 +0000

    minor fix to form styling

--- curo/pages/report-pages/economicReport.js
+++ curo/pages/report-pages/economicReport.js
@@ -40,24 +40,69 @@ export default function Ecconomic() {
             
             <div className={styles.dashboardContent}>
                 <div style={{background:'#F8F8F8', width:'100%', height:'fit-content', boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.3)', borderRadius:30, padding:30}}>
-                    <div>
-                        <p style={{fontSize:18}}>1. The team met the sprint's defined goals and objectives within the allocated budget</p>
-                        <Likert {...likertOptions} />
+                    <p style={{fontSize:22, fontWeight:600, marginBottom: 40, color:'black'}}>Please rank the following statements based on how much you agree with them:</p>
+                    <div style={{width:'75%'}}>
+                        <div>
+                            <p style={{fontSize:18}}>1. The team met the sprint's defined goals and objectives within the allocated budget</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>2. Initiatives were in place to ensure job security for team members</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>3. There were professional development opportunities for the team members</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>4. The team supports learning and career advancement for team members</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>5. Compensation structures within the team were fair</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>6. The project positively contributed to economic growth in its community or industry</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>7. The team actively sought ways to contribute to the economic wellbeing of stakeholders</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>8. Efforts were made to minimize the environmental impact of the project's infrastructure</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>9. The team invested in innovative technologies or practices to improve efficiency</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>10. Innovation was a priority in project planning and execution</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>11. The team actively engages in activities that enhance the competitiveness of the industry</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>12. Resources, including time and personnel, were allocated efficiently throughout the project</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>13. The team conducted cost-benefit analyses for major project decisions</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>14. The team prioritizes activities with a positive cost-benefit ratio</p>
+                            <Likert {...likertOptions} />
+                        </div>
+                        <div>
+                            <p style={{fontSize:18}}>15. Decisions were made with a focus on maximizing the project's economic benefits</p>
+                            <Likert {...likertOptions} />
+                        </div>
                     </div>
-                    <p>2. Initiatives were in place to ensure job security for team members</p>
-                    <p>3. There were professional development opportunities for the team members</p>
-                    <p>4. The team supports learning and career advancement for team members</p>
-                    <p>5. Compensation structures within the team were fair</p>
-                    <p>6. The project positively contributed to economic growth in its community or industry</p>
-                    <p>7. The team actively sought ways to contribute to the economic wellbeing of stakeholders</p> 
-                    <p>8. Efforts were made to minimize the environmental impact of the project's infrastructure</p>
-                    <p>9. The team invested in innovative technologies or practices to improve efficiency</p>
-                    <p>10. Innovation was a priority in project planning and execution</p> 
-                    <p>11. The team actively engages in activities that enhance the competitiveness of the industry</p>    
-                    <p>12. Resources, including time and personnel, were allocated efficiently throughout the project</p> 
-                    <p>13. The team conducted cost-benefit analyses for major project decisions</p> 
-                    <p>14. The team prioritizes activities with a positive cost-benefit ratio</p>     
-                    <p>15. Decisions were made with a focus on maximizing the project's economic benefits</p>
                 </div>
             </div>
 
