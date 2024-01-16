61f87e3398b029924702b883ef93d6d69e9ccb6f
Niamh Boyle <2566489b@student.gla.ac.uk>
2024-01-16 20:41:01 +0000

    created a stylesheet for report pages

--- curo/pages/report-pages/economicReport.js
+++ curo/pages/report-pages/economicReport.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
+import reportStyles from '../../styles/Reports.module.css';
 import "@fontsource/montserrat";
 import '@fontsource-variable/karla';
 import "@fontsource/manrope";
@@ -87,122 +88,102 @@ export default function Economic() {
             <Header title="Economic"/>
             
             <div className={styles.dashboardContent}>
-                <div style={{background:'#F8F8F8', width:'100%', height:'fit-content', boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.3)', borderRadius:30, padding:30, overflow:'auto'}}>
-                    <div style={{width:'75%', display: questionView === "quantitative" ? "block" : "none"}}>
-                      <p style={{fontSize:22, fontWeight:600, marginBottom: 40, color:'black'}}>Please rank the following statements based on how much you agree with them:</p>
+                <div className={reportStyles.reportContainer}>
+                    <div style={{width:'90%', display: questionView === "quantitative" ? "block" : "none"}}>
+                      <div className={reportStyles.headingText}>Please rank the following statements based on how much you agree with them:</div>
                       <div>
-                          <p style={{fontSize:18}}>1. The team met the sprint's defined goals and objectives within the allocated budget</p>
+                          <p>1. The team met the sprint's defined goals and objectives within the allocated budget</p>
                           <Likert {...likertOptions} id='1' onChange={(val) => handleLikertChange(val.value, "q1")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>2. Initiatives were in place to ensure job security for team members</p>
+                          <p>2. Initiatives were in place to ensure job security for team members</p>
                           <Likert {...likertOptions} id='2' onChange={(val) => handleLikertChange(val.value, "q2")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>3. There were professional development opportunities for the team members</p>
+                          <p>3. There were professional development opportunities for the team members</p>
                           <Likert {...likertOptions} id='3' onChange={(val) => handleLikertChange(val.value, "q3")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>4. The team supports learning and career advancement for team members</p>
+                          <p>4. The team supports learning and career advancement for team members</p>
                           <Likert {...likertOptions} id='4' onChange={(val) => handleLikertChange(val.value, "q4")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>5. Compensation structures within the team were fair</p>
+                          <p>5. Compensation structures within the team were fair</p>
                           <Likert {...likertOptions} id='5' onChange={(val) => handleLikertChange(val.value, "q5")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>6. The project positively contributed to economic growth in its community or industry</p>
+                          <p>6. The project positively contributed to economic growth in its community or industry</p>
                           <Likert {...likertOptions} id='6' onChange={(val) => handleLikertChange(val.value, "q6")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>7. The team actively sought ways to contribute to the economic wellbeing of stakeholders</p>
+                          <p>7. The team actively sought ways to contribute to the economic wellbeing of stakeholders</p>
                           <Likert {...likertOptions} id='7' onChange={(val) => handleLikertChange(val.value, "q7")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>8. Efforts were made to minimize the environmental impact of the project's infrastructure</p>
+                          <p>8. Efforts were made to minimize the environmental impact of the project's infrastructure</p>
                           <Likert {...likertOptions} id='8' onChange={(val) => handleLikertChange(val.value, "q8")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>9. The team invested in innovative technologies or practices to improve efficiency</p>
+                          <p>9. The team invested in innovative technologies or practices to improve efficiency</p>
                           <Likert {...likertOptions} id='9' onChange={(val) => handleLikertChange(val.value, "q9")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>10. Innovation was a priority in project planning and execution</p>
+                          <p>10. Innovation was a priority in project planning and execution</p>
                           <Likert {...likertOptions} id='10' onChange={(val) => handleLikertChange(val.value, "q10")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>11. The team actively engages in activities that enhance the competitiveness of the industry</p>
+                          <p>11. The team actively engages in activities that enhance the competitiveness of the industry</p>
                           <Likert {...likertOptions} id='11' onChange={(val) => handleLikertChange(val.value, "q11")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>12. Resources, including time and personnel, were allocated efficiently throughout the project</p>
+                          <p>12. Resources, including time and personnel, were allocated efficiently throughout the project</p>
                           <Likert {...likertOptions} id='12' onChange={(val) => handleLikertChange(val.value, "q12")} />
                       </div>
                       
                       <div>
-                          <p style={{fontSize:18}}>13. The team conducted cost-benefit analyses for major project decisions</p>
+                          <p>13. The team conducted cost-benefit analyses for major project decisions</p>
                           <Likert {...likertOptions} id='13' onChange={(val) => handleLikertChange(val.value, "q13")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>14. The team prioritizes activities with a positive cost-benefit ratio</p>
+                          <p>14. The team prioritizes activities with a positive cost-benefit ratio</p>
                           <Likert {...likertOptions} id='14' onChange={(val) => handleLikertChange(val.value, "q14")} />
                       </div>
                       <div>
-                          <p style={{fontSize:18}}>15. Decisions were made with a focus on maximizing the project's economic benefits</p>
+                          <p>15. Decisions were made with a focus on maximizing the project's economic benefits</p>
                           <Likert {...likertOptions} id='15' onChange={(val) => handleLikertChange(val.value, "q15")} />
                       </div>
                       <button
                           disabled={nextAvailable}
                           onClick={handleSubmitNew}
-                          className={styles.ReportBtn}
+                          className={reportStyles.nextButton}
                           style={{
                             background: !nextAvailable ? "#18392B" : "gray",
                             cursor: !nextAvailable ? "pointer" : "default",
-                            width: "fit-content",
-                            borderRadius: "30px",
-                            fontFamily:"Montserrat",
-                            fontSize:"22px",
-                            color:"white",
-                            padding:"15px",
-                            border:"none",
-                            float:"right",
                           }}>
                           Continue &rarr;
                       </button>
                     </div>
-                    <div style={{width:"75%", display: questionView === "qualitative" ? "block" : "none"}}>
-                      <p style={{fontSize:22, fontWeight:600, marginBottom: 40, color:'black'}}>Please provide feedback in the following areas where appropriate:</p>
+                    <div style={{width:"90%", display: questionView === "qualitative" ? "block" : "none"}}>
+                      <div className={reportStyles.headingText}>Please provide feedback in the following areas where appropriate:</div>
                       <div>
-                        <p style={{fontSize:18}}>1. Were there any cost overruns, and what were the contributing factors?</p>
+                        <p>1. Were there any cost overruns, and what were the contributing factors?</p>
                         <input 
                           onChange={(e) => setQualResponses({...qualResponses, q1:e.target.value})} 
-                          style={{width: '100%', height: 48, display:'block', borderRadius: 8, border: '1px #CDCDCD solid', margin:5, fontSize: 16, fontFamily: 'Karla Variable', padding:10}} 
+                          className={reportStyles.inputBoxes}
                           >
                         </input>
                       </div>
                       <div>
-                        <p style={{fontSize:18}}>2. What efforts did the team make to reduce technical debt?</p>
+                        <p>2. What efforts did the team make to reduce technical debt?</p>
                         <input 
                           onChange={(e) => setQualResponses({...qualResponses, q2:e.target.value})} 
-                          style={{width: '100%', height: 48, display:'block', borderRadius: 8, border: '1px #CDCDCD solid', margin:5, fontSize: 16, fontFamily: 'Karla Variable', padding:10}} 
+                          className={reportStyles.inputBoxes}
                           >
                         </input>
                       </div>
                       <button
                           onClick={handleSubmitNew}
-                          className={styles.ReportBtn}
-                          style={{
-                            background: "#18392B",
-                            cursor: "pointer",
-                            width: "fit-content",
-                            borderRadius: "30px",
-                            fontFamily:"Montserrat",
-                            fontSize:"22px",
-                            color:"white",
-                            padding:"15px",
-                            border:"none",
-                            float:"right",
-                          }}>
+                          className={reportStyles.nextButton}>
                           Submit
                       </button>
                     </div>
