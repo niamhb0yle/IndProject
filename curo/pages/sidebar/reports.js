2fbfddfdd580b737171319c31fcd4a21704e5c9a
Niamh Boyle <2566489b@student.gla.ac.uk>
2024-02-01 00:37:11 +0000

    added drag and drop feature for issue board - initially done in reports.js

--- curo/pages/sidebar/reports.js
+++ curo/pages/sidebar/reports.js
@@ -6,8 +6,48 @@ import '@fontsource-variable/karla';
 import "@fontsource/manrope";
 import Header from '../../components/Header';
 import SideBar from '../../components/sidebar';
+import { useState } from 'react';
+import ibStyles from '../../styles/IssueBoard.module.css';
 
-export default function Homepage() {
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const initialTasks = {
+  'To Do:': ['Task 1', 'Task 2', 'Task 3'],
+  'In Progress:': [],
+  'Complete': []
+};
+
+export default function Reports() {
+
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const onDragEnd = result => {
+    const { source, destination } = result;
+
+    // Dropped outside the list
+    if (!destination) {
+      return;
+    }
+
+    if (source.droppableId === destination.droppableId) {
+      const items = Array.from(tasks[source.droppableId]);
+      const [reorderedItem] = items.splice(source.index, 1);
+      items.splice(destination.index, 0, reorderedItem);
+
+      setTasks({ ...tasks, [source.droppableId]: items });
+    } else {
+      const sourceItems = Array.from(tasks[source.droppableId]);
+      const [movedItem] = sourceItems.splice(source.index, 1);
+      const destinationItems = Array.from(tasks[destination.droppableId]);
+      destinationItems.splice(destination.index, 0, movedItem);
+
+      setTasks({ 
+        ...tasks, 
+        [source.droppableId]: sourceItems,
+        [destination.droppableId]: destinationItems 
+      });
+    }
+  };
 
   return (
     <div>
@@ -24,7 +64,37 @@ export default function Homepage() {
             <Header title="Reports"/>
 
             <div className={styles.dashboardContent}>
-              <p>View and export your previous reports below.</p>
+            <div className={ibStyles.boardContainer}>
+            <DragDropContext onDragEnd={onDragEnd}>
+              {Object.keys(tasks).map((columnId) => (
+                <Droppable key={columnId} droppableId={columnId}>
+                  {(provided, snapshot) => (
+                    <div
+                    className={ibStyles.issueColumn}
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      <h3 className={ibStyles.columnHeader}>{columnId}</h3>
+                      {tasks[columnId].map((item, index) => (
+                        <Draggable key={item} draggableId={item} index={index}>
+                          {(provided, snapshot) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              {item}
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+      ))}
+    </DragDropContext>
+    </div>
             </div>
 
           </div>
